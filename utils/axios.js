05eeccf8875221e3c8f39e606b12c025a0aa3857
muharisharis13 <muharisharis13@gmail.com>
2023-01-26 13:21:06 +0700

    move base url

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "./cookie"
 
-const baseURL = `${process.env.NEXT_PUBLIC_BASE_URL}api`;
+const baseURL = `https://dashboard-app-nextjs.vercel.app/api`;
 const timeout = 16000;
 
 const instance = axios.create({
